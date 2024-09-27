0f394fe41fcfe688522203d9e4407cdff4df10e0
Danielhhgfj <henrique.matheus.souza@escola.pr.gov.br>
2024-09-27 09:16:30 -0300

    asdasdsa

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ return lado**2;
 console.log (areaQuadrado(6));
 
 
-let media = function (media){
-return (1+2+3)/3;
+let media = function (num1,num2,num3,){
+return (num1+num2+num3)/3;
 }
 console.log(media(1+2+3/3));
 
