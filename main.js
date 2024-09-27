1219d45d546a0391668dc9fc52032765f1834313
Danielhhgfj <henrique.matheus.souza@escola.pr.gov.br>
2024-09-27 09:38:26 -0300

    asfasfasf

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ console.log(soma(5,3));
 function areaQuadrado(lado){
 return lado**2;
 }
+let valoresmedia=parseInt(prompt('insira o valor'))
 console.log (areaQuadrado(6));
-
 
 let media = function (num1,num2,num3){
 return (num1+num2+num3)/3;
